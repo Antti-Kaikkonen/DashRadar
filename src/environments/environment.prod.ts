b8e81ba243ba4558282211f15a29964574f44a00
Antti-Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-07-15 15:22:36 +0300

    Changed insight api url to work with new version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  insightApiUrl: 'https://dashradar.com/insight-api-dash',
+  insightApiUrl: 'https://dashradar.com/insight-api',
   insightSocketUrl: 'https://dashradar.com',
   cypherUrl: 'https://dashradar.com/db/data/read-only_query',
   chartJsImageURL: 'https://dashradar.com'
