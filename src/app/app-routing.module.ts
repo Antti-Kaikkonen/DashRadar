0f779e88906e8f2a49bbe9012c273b38e69b1c0f
Antti-Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-10-22 20:54:40 +0300

    Added a simple landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: 'graph', loadChildren: './graph/graph.module#GraphModule' },
   { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
   { path: 'explorer', loadChildren: './explorer/explorer.module#ExplorerModule' },
   { path: 'nodes', loadChildren: './nodes/nodes.module#NodesModule' },
   { path: 'cypher', redirectTo: '/charts/editor?query=:query', pathMatch: 'prefix'},
-  { path: '', loadChildren: './graph/graph.module#GraphModule' },
+  { path: '', component: DashboardComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatTabsModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatIconModule, MatTabsModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -40,6 +40,7 @@ import { AddressService } from './addresses/address.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockService } from './blocks/block.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarModule } from './navbar/navbar.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TransactionService } from './transactions/transaction.service';
@@ -47,9 +48,11 @@ import { TransactionService } from './transactions/transaction.service';
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
+    MatCardModule,
     NavbarModule,
     FlexLayoutModule,
     BrowserModule,
