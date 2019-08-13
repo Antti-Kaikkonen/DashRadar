fc29094701afe924ae5552710f12363bd7d80d2d
Antti-Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2019-08-14 01:34:20 +0300

    disabled nodes page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 export const routes: Routes = [
   { path: 'graph', loadChildren: './graph/graph.module#GraphModule', data: { title: 'Graph' } },
   { path: 'charts', loadChildren: './charts/charts.module#ChartsModule', data: { title: 'Charts' } },
   { path: 'explorer', loadChildren: './explorer/explorer.module#ExplorerModule', data: { title: 'Explorer' } },
-  { path: 'nodes', loadChildren: './nodes/nodes.module#NodesModule', data: { title: 'Nodes' } },
+  //{ path: 'nodes', loadChildren: './nodes/nodes.module#NodesModule', data: { title: 'Nodes' } },
   { path: 'cypher', redirectTo: '/charts/editor?query=:query', pathMatch: 'prefix'},
   { path: '', component: DashboardComponent, data: { title: 'DashRadar' } },
   { path: '**', component: PageNotFoundComponent }
